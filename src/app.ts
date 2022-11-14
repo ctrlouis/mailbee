91968e78ba8fc1a6427cc8504013efbe69271bf9
ctrlouis <louis.bty57@gmail.com>
2022-11-14 23:35:36 +0100

    ✨ Add morgan logs

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import morgan from 'morgan';
 import { SendForm } from './controllers/FormsToMail';
 
 const app = express();
 const router = express.Router();
 app.use(express.json());
+app.use(morgan('dev'));
 
 router.get('/', (req: Request, res: Response) => {
     res.send("Welcome to mail bee! 🐝");
