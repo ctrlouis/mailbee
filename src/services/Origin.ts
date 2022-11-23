f3d9f6a18c06c5deab508ae6ed771d8778ea8fa0
ctrlouis <louis.bty57@gmail.com>
2022-11-23 14:17:14 +0100

    🐛 Fix request from https origin

--- src/services/Origin.ts
+++ src/services/Origin.ts
@@ -1,6 +1,8 @@
 export function allowOrigin(requestOrigin :string, allowedDomaines :string[]) :boolean {
     let allow = false;
-    const findDomain :string|null|undefined = allowedDomaines.find(currentDomain => currentDomain === requestOrigin.replace("http://", ''));
+    const findDomain :string|null|undefined = allowedDomaines.find(currentDomain => {
+        return currentDomain === requestOrigin.replace("http://", '') || currentDomain === requestOrigin.replace("https://", '')
+    });
     if (typeof findDomain === 'string') allow = true; 
     return allow;
 }
